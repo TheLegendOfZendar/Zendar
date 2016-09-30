0405335fa51ae294238d1c98a922fb208aea2b47
Savaş C. Coruh <cradonwar@gmail.com>
2016-09-30 13:35:08 +0300

    Restored Repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Legend of Zendar
+Source for The Legend of Zendar, a mod for Crusader Kings II.
---
+++ The Legend of Zendar/common/defines/00_defines.lua
@@ -0,0 +1,42 @@
+-- LIFESPAN
+NDefines.NCharacter.AGE_OLD = 60									-- Non-child characters below this age have the 'adult' static modifier applied
+NDefines.NCharacter.AGE_VERY_OLD = 80								-- Non-child characters below this age have the 'old' static modifier applied
+NDefines.NCharacter.MAX_CHILD_BIRTH_AGE = 51						-- Female menopause age.
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_50 = 75				-- Natural deaths per decade out of 10000 people: Age 50-59
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_60 = 250				-- Natural deaths per decade out of 10000 people: Age 60-69
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_70 = 1000				-- Natural deaths per decade out of 10000 people: Age 70-79
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_80 = 2500				-- Natural deaths per decade out of 10000 people: Age 80-90
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_90 = 6000				-- Natural deaths per decade out of 10000 people: Age 90-99
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_100 = 8000				-- Natural deaths per decade out of 10000 people: Age 100+
+--
+-- DYNASTIC SCOREBOARD
+NDefines.NEndGame.DYN1_SCORE = 100000
+NDefines.NEndGame.DYN1_ID = 100
+NDefines.NEndGame.DYN2_SCORE = 90000
+NDefines.NEndGame.DYN2_ID = 1001
+NDefines.NEndGame.DYN3_SCORE = 80000
+NDefines.NEndGame.DYN3_ID = 1000
+NDefines.NEndGame.DYN4_SCORE = 70000
+NDefines.NEndGame.DYN4_ID = 2000
+NDefines.NEndGame.DYN5_SCORE = 60000
+NDefines.NEndGame.DYN5_ID = 2001
+NDefines.NEndGame.DYN6_SCORE = 50000
+NDefines.NEndGame.DYN6_ID = 6000
+NDefines.NEndGame.DYN7_SCORE = 40000
+NDefines.NEndGame.DYN7_ID = 6001
+NDefines.NEndGame.DYN8_SCORE = 30000
+NDefines.NEndGame.DYN8_ID = 5000
+NDefines.NEndGame.DYN9_SCORE = 20000
+NDefines.NEndGame.DYN9_ID = 3000
+NDefines.NEndGame.DYN10_SCORE = 15000
+NDefines.NEndGame.DYN10_ID = 3001
+NDefines.NEndGame.DYN11_SCORE = 10000
+NDefines.NEndGame.DYN11_ID = 4000
+NDefines.NEndGame.DYN12_SCORE = 7500
+NDefines.NEndGame.DYN12_ID = 4001
+NDefines.NEndGame.DYN13_SCORE = 5000
+NDefines.NEndGame.DYN13_ID = 2021
+NDefines.NEndGame.DYN14_SCORE = 2000
+NDefines.NEndGame.DYN14_ID = 1009
+NDefines.NEndGame.DYN15_SCORE = 1000
+NDefines.NEndGame.DYN15_ID = 4017
